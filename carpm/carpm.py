675db5b5d5b042c27d3b5dc604b239c08ed71f2e
Kshitij Mittal <kmittal@Kshitijs-MacBook-Pro.local>
2017-04-05 22:06:09 +0530

    initial blank commit

---
+++ carpm/carpm.py
@@ -0,0 +1,5 @@
+import requests
+
+class CarpmClient(object):
+    def __init__(self):
+        return
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+requirements = [
+    "requests"
+]
+
+setup(
+    name='carpm',
+    version='0.1.0',
+    description="",
+    long_description='',
+    author="Kshitij Mittal",
+    author_email='kshitij@loanzen.in',
+    url='https://github.com/loanzen/carpm-py',
+    packages=[
+        'carpm',
+    ],
+    package_dir={'carpm':
+                 'carpm'},
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+)
